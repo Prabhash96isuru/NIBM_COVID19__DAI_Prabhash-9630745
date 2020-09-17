b87d6cee25910c800014a582a7a5c7e66880003b
isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-17 16:06:26 -0700

    setup viewControllers
    Login & Create Account

---
+++ NIBM_COVID19/ViewControllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  NIBM_COVID19
+//
+//  Created by Isuru Prabhash on 9/17/20.
+//  Copyright © 2020 Isuru Prabhash. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
