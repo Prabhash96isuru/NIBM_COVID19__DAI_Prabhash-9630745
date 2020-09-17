1cd6bfdcd8eb1e05a10602a7dd6c577b1e259338
isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-17 12:50:11 -0700

    Initial commit

---
+++ NIBM_COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM_COVID19
+//
+//  Created by Isuru Prabhash on 9/17/20.
+//  Copyright © 2020 Isuru Prabhash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NIBM_COVID19Tests/NIBM_COVID19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  NIBM_COVID19Tests.swift
+//  NIBM_COVID19Tests
+//
+//  Created by Isuru Prabhash on 9/17/20.
+//  Copyright © 2020 Isuru Prabhash. All rights reserved.
+//
+
+import XCTest
+@testable import NIBM_COVID19
+
+class NIBM_COVID19Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
