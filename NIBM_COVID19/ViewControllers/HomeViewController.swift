6facdcec365d833e0690dacda2a8f0f2e83116a3
isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-20 10:01:47 -0700

    adding map

--- NIBM_COVID19/ViewControllers/HomeViewController.swift
+++ NIBM_COVID19/ViewControllers/HomeViewController.swift
@@ -7,24 +7,48 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController,CLLocationManagerDelegate {
 
+    @IBOutlet var mapView : MKMapView!
+    
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
-    */
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first
+        {
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    func render(_ location: CLLocation)
+    {
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        
+        let region	= MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
 }
+	
