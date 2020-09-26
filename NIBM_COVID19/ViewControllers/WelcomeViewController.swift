95affe04d13172056d910cf63ee2de8c13f6188c
isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-26 13:38:29 -0700

    fixing the blackscreen error

--- NIBM_COVID19/ViewControllers/WelcomeViewController.swift
+++ NIBM_COVID19/ViewControllers/WelcomeViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WelcomeViewController: UITabBarController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
